be322a73e44a743a516c55424a41e1d8a0020fd1
George Svitenko <georg.svit@gmail.com>
2020-05-29 16:12:00 +0300

    Добавьте файлы проекта.

---
+++ Net-GL-BaseCamp.Choice/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Net_GL_BaseCamp.Choice.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
